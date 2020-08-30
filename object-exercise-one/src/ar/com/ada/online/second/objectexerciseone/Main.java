f3c89ba0148480accfe3a1cde8da6d8b4766f9f4
Sabrina Colman <sabrina-rct@hotmail.com>
2020-08-29 21:04:11 -0300

    finish object exercise one

--- object-exercise-one/src/ar/com/ada/online/second/objectexerciseone/Main.java
+++ object-exercise-one/src/ar/com/ada/online/second/objectexerciseone/Main.java
@@ -4,32 +4,39 @@ public class Main {
 
     public static void main(String[] args) {
         //se crean el objeto de tipo Animal
-        Animal aninalA = new Animal();
-        // se crean el objeto de tipo Vehicule
+        Animal animalA = new Animal();
+        // se crean el objeto de tipo Vehiculo
         Vehicle carA = new Vehicle();
         // se crean el objeto de tipo Person
         Person personA = new Person();
 
         // settear los attr
-        aninalA.breed = "mestizo";
-        aninalA.color = "Black";
-        aninalA.name = "Palito";
+        animalA.breed = "mestizo";
+        animalA.color = "Black";
+        animalA.name = "Palito";
 
         carA.color = "Rojo";
         carA.maxSpeed = 160;
         carA.brand = "Citroen-Cactus";
 
-        personA.name ="Rosa";
+        personA.name = "Rosa";
         personA.lastName = "Torres";
         personA.age = 32;
 
-        System.out.println(aninalA.breed + " " + aninalA.color + " " + aninalA.name);
+
+        System.out.println(animalA.breed + " " + animalA.color + " " + animalA.name);
+        animalA.eat();
+        animalA.move();
+        animalA.sleep();
+        System.out.println();
         System.out.println(carA.brand + " " + carA.maxSpeed + " " + carA.color);
+        carA.moveOn();
+        carA.brake();
+        System.out.println();
         System.out.println(personA.name + " " + personA.lastName + " " + personA.age);
-
-
-
-
+        personA.play();
+        personA.talk();
+        personA.play();
 
 
     }
